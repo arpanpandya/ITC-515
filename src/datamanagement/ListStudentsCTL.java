c37dc28c33c59cfa14b02527f7f20de9aa9003f2
RizvanSaiyed <r.saiyed8054@gmail.com>
2016-09-08 11:24:49 +0530

    Update ListStudentsCTL.java

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -4,5 +4,6 @@ private StudentManager sm;
 public ListStudentsCTL() {sm = StudentManager.get();}
             public void listStudents( IStudentLister lister, String unitCode ) {
     lister.clearStudents();
+    //create new object with student name 
                 StudentMap students = sm.getStudentsByUnit( unitCode );
 for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
